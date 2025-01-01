500ff7ebe25a9a0f2b873fbcf311657a04d14b07
KarmaX4 <99162752+KarmaX4@users.noreply.github.com>
2025-01-01 18:39:25 +0530

    base url updated

--- frontend/src/app/auth.ts
+++ frontend/src/app/auth.ts
@@ -19,9 +19,10 @@ declare module "next-auth" {
 
 const login = async (credentials: { email: string; password: string }) => {
   try {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/';
     // console.log("credentials =",credentials);
     const response = await fetch(
-      ` http://localhost:5000/api/auth/login`,
+      `${baseUrl}/api/auth/login`,
       {
         method: "POST",
         headers: {
